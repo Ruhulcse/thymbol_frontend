53656c08140c43141eee9e25085e93893608686e
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-05-27 00:31:43 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+// home pages  & dashboard
+//import Dashboard from "./pages/dashboard";
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const Login = lazy(() => import('./pages/auth/login'));
+
+import Layout from './layout/Layout';
+import AuthLayout from './layout/AuthLayout';
+
+function App() {
+    return (
+        <main className="App  relative">
+            <Routes>
+                {/* <Route path="/" element={<AuthLayout />}> */}
+                    <Route path="/login" element={<Login />} />
+                {/* </Route> */}
+                <Route path="/*" element={<Layout />}>
+                    <Route path="dashboard" element={<Dashboard />} />
+                </Route>
+            </Routes>
+        </main>
+    );
+}
+
+export default App;
