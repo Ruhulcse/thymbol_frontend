a81cd00341548cb5b9d2954a9e8add2bbad691c8
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-06-08 20:49:27 +0600

    rename

--- src/components/partials/header/Tools/Logo.jsx
+++ src/components/partials/header/Tools/Logo.jsx
@@ -3,8 +3,8 @@ import useDarkMode from "@/hooks/useDarkMode";
 import { Link } from "react-router-dom";
 import useWidth from "@/hooks/useWidth";
 
-import MainLogo from "@/assets/images/logo/logo-c.png.png";
-import LogoWhite from "@/assets/images/logo/logo-c.png.png";
+import MainLogo from "@/assets/images/logo/logo-c.png";
+import LogoWhite from "@/assets/images/logo/logo-c.png";
 import MobileLogo from "@/assets/images/logo/logo-c.png";
 import MobileLogoWhite from "@/assets/images/logo/logo-c-white.png";
 const Logo = () => {
