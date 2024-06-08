aabb1cfc0d3076637f9445506b752a34f821c56a
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-06-08 20:45:06 +0600

    fix logo import

--- src/components/partials/header/Tools/Logo.jsx
+++ src/components/partials/header/Tools/Logo.jsx
@@ -3,7 +3,7 @@ import useDarkMode from "@/hooks/useDarkMode";
 import { Link } from "react-router-dom";
 import useWidth from "@/hooks/useWidth";
 
-import MainLogo from "@/assets/images/logo/logo.png";
+import MainLogo from "@/assets/images/logo/logo-white.png";
 import LogoWhite from "@/assets/images/logo/logo-white.png";
 import MobileLogo from "@/assets/images/logo/logo-c.png";
 import MobileLogoWhite from "@/assets/images/logo/logo-c-white.png";
