39cc06b044a293acf20efec5919e2e46be56e8f7
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-07-01 00:53:21 +0600

    fix

--- src/components/modalVideo/index.jsx
+++ src/components/modalVideo/index.jsx
@@ -9,9 +9,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import img from './../../assets/images/merchant/Group 1000007830.png';
 
 const data = [
-    { img: img, videoId: "L61p2uyiMSo" },
-    { img: img, videoId: "K4TOrB7at0Y" },
-    { img: img, videoId: "q3J0BqkZgNM" }
+    { img: img, videoId: 'L61p2uyiMSo' },
+    { img: img, videoId: 'K4TOrB7at0Y' },
+    { img: img, videoId: 'q3J0BqkZgNM' },
 ];
 
 function ModalsVideo() {
@@ -25,39 +25,43 @@ function ModalsVideo() {
 
     return (
         <div className="relative lg:h-[536px] lg:w-[1000px] w-[140px] h-[135px] sm:w-[180px] sm:h-[186px] md:w-[230px] md:h-[240px] px-10">
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={1}
-                onSlideChange={() => 
-                onSwiper={(swiper) => 
-                navigation={true}
-                loop={true}
-                modules={[Navigation]}
-            >
-                {data.map((item, index) => (
-                    <SwiperSlide key={index}>
-                        <div className="relative w-full h-full">
-                            <img src={item.img} alt="" className="h-full w-full" />
-                            <div
-                                className="absolute z-50 inset-0 flex items-center justify-center cursor-pointer"
-                                onClick={() => openModal(item.videoId)}
-                            >
-                                <Icon
-                                    className="text-white text-5xl"
-                                    icon="heroicons:play-circle"
+            <>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    navigation={true}
+                    loop={true}
+                    modules={[Navigation]}
+                >
+                    {data.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <div className="relative w-full h-full">
+                                <img
+                                    src={item.img}
+                                    alt=""
+                                    className="h-full w-full"
                                 />
+                                <div
+                                    className="absolute z-50 inset-0 flex items-center justify-center cursor-pointer"
+                                    onClick={() => openModal(item.videoId)}
+                                >
+                                    <Icon
+                                        className="text-white text-5xl"
+                                        icon="heroicons:play-circle"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <ModalVideo
-                channel="youtube"
-                youtube={{ mute: 0, autoplay: 0 }}
-                isOpen={isOpen}
-                videoId={currentVideoId}
-                onClose={() => setOpen(false)}
-            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <ModalVideo
+                    channel="youtube"
+                    youtube={{ mute: 0, autoplay: 0 }}
+                    isOpen={isOpen}
+                    videoId={currentVideoId}
+                    onClose={() => setOpen(false)}
+                />
+            </>
         </div>
     );
 }
