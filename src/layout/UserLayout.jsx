8f68e48c8b700948823f0c223d60ad1c8091b025
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-07-15 01:17:13 +0600

    fix

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,18 +1,17 @@
+import { food } from '@/assets/images/home';
 import Loading from '@/components/Loading';
 import Profile from '@/components/partials/header/Tools/Profile';
 import Button from '@/components/ui/Button';
 import { navLink } from '@/constant/data';
 import useCurrentWidth from '@/hooks/useCurrentWidth';
 import { selectCurrentUser, setUser } from '@/store/api/auth/authSlice';
+import { getUser } from '@/store/api/user/userSlice';
 import { Icon } from '@iconify/react';
 import { motion } from 'framer-motion';
 import { Suspense, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, Outlet } from 'react-router-dom';
 import logo from './../assets/images/home/Thymbol Logo.png';
-import { food } from '@/assets/images/home';
-import { useDispatch } from 'react-redux';
-import { getUser } from '@/store/api/user/userSlice';
 
 function UserLayout() {
     const [activeIndex, setActiveIndex] = useState(0);
@@ -25,21 +24,20 @@ function UserLayout() {
     useEffect(() => {
         const localAuth = localStorage?.getItem('auth');
         if (localAuth) {
-          const auth = JSON.parse(localAuth);
-          if (auth?.accessToken) {
-            dispatch(
-              setUser({
-                token: auth.accessToken,
-                user_id: auth.user_id,
-                isLoggedIn: true,
-                userType: auth.userType
-              })
-            );
-            dispatch(getUser({ user_id: auth.user_id }));
-          }
+            const auth = JSON.parse(localAuth);
+            if (auth?.accessToken) {
+                dispatch(
+                    setUser({
+                        token: auth.accessToken,
+                        user_id: auth.user_id,
+                        isLoggedIn: true,
+                        userType: auth.userType,
+                    })
+                );
+                dispatch(getUser({ user_id: auth.user_id }));
+            }
         }
-        
-      }, [dispatch]);
+    }, [dispatch]);
 
     useEffect(() => {
         setSidebar(currentWidth >= 1024 ? true : false);
@@ -66,23 +64,28 @@ function UserLayout() {
                     </div>
                     <div
                         className={`flex absolute  lg:relative  left-0 flex-col lg:flex-row top-0 lg:bg-blue-400 bg-white  z-[100] rounded-lg shadow-lg lg:shadow-none w-64 h-screen lg:h-min lg:w-max ${
-                            sidebar
-                                ? '-translate-x-[0px]'
-                                : 'translate-x-[-9999px]'
+                            sidebar && '-translate-x-[0px]'
+                            // : 'translate-x-[-9999px]'
                         }`}
                     >
-                         <Icon
-                        className="absolute top-2 right-2 text-black-500 text-2xl cursor-pointer  lg:hidden"
-                        icon={`heroicons:x-mark`} onClick={()=>setSidebar(null)}
-                    />
-                        <div className='h-24 w-24 bg-green-500 rounded-full my-4 mx-4 lg:hidden'><img src={food} className='h-full w-full object-cover'/></div>
+                        <Icon
+                            className="absolute top-2 right-2 text-black-500 text-2xl cursor-pointer  lg:hidden"
+                            icon={`heroicons:x-mark`}
+                            onClick={() => setSidebar(null)}
+                        />
+                        <div className="h-24 w-24 bg-green-500 rounded-full my-4 mx-4 lg:hidden">
+                            <img
+                                src={food}
+                                className="h-full w-full object-cover"
+                            />
+                        </div>
                         {navLink?.map((item, i) => (
                             <Link
                                 key={i}
                                 to={item.link}
                                 onClick={() => {
                                     setActiveIndex(i);
-                                    setSidebar(null)
+                                    setSidebar(null);
                                 }}
                                 className={` px-6 rounded-md lg:hover:text-black-500 hover:text-white hover:bg-black-500 lg:hover:bg-white py-2 m-1 ${
                                     activeIndex === i
@@ -90,21 +93,43 @@ function UserLayout() {
                                         : 'lg:text-white'
                                 } `}
                             >
-                              <span className='flex items-center '>{<Icon icon={item.icon} className='mx-2 text-xl lg:hidden'/>} {item.title}</span>  
+                                <span className="flex items-center ">
+                                    {
+                                        <Icon
+                                            icon={item.icon}
+                                            className="mx-2 text-xl lg:hidden"
+                                        />
+                                    }{' '}
+                                    {item.title}
+                                </span>
                             </Link>
                         ))}
-                       
+
                         <Link
                             to={'/'}
                             className="px-6 hover:text-white hover:bg-black-500 rounded-md py-2 m-1 text-black-500 md:hidden block"
                         >
-                            <span className='flex items-center'><Icon icon={'heroicons:user'} className='mx-2 text-xl'/>Profile</span>
+                            <span className="flex items-center">
+                                <Icon
+                                    icon={'heroicons:user'}
+                                    className="mx-2 text-xl"
+                                />
+                                Profile
+                            </span>
                         </Link>
                         <Link
                             to={'/'}
                             className="px-6 hover:text-white hover:bg-black-500 rounded-md py-2 m-1 text-black-500 md:hidden block"
                         >
-                            <span className='flex items-center'><Icon icon={'heroicons:arrow-right-start-on-rectangle'} className='mx-2 text-xl'/>Log Out</span>
+                            <span className="flex items-center">
+                                <Icon
+                                    icon={
+                                        'heroicons:arrow-right-start-on-rectangle'
+                                    }
+                                    className="mx-2 text-xl"
+                                />
+                                Log Out
+                            </span>
                         </Link>
                     </div>
                     <div className="hidden md:flex">
