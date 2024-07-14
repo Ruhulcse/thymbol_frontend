2848236d7bc5bd74db0d011610f0b62c99875446
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-07-14 18:37:19 +0600

    remove react-refresh [redundent]

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import rollupReplace from "@rollup/plugin-replace";
 import react from "@vitejs/plugin-react";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+// import reactRefresh from "@vitejs/plugin-react-refresh";
 import path from "path";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import preload from "vite-plugin-preload";
 
 // https://vitejs.dev/config/
 
@@ -69,8 +68,7 @@ export default defineConfig({
       },
     }),
     react(),
-    reactRefresh(),
+    // reactRefresh(),
     VitePWA(manifestForPlugIn),
-    preload()
   ],
 });
